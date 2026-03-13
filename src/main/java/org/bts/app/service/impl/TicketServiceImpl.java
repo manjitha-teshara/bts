1c749f2c008b896938c45310b9440d2a4950cb18
Manjitha Teshara <teranteshara01@gmail.com>
2026-03-13 09:01:33 +0530

    refactor static methods

--- src/main/java/org/bts/app/service/impl/TicketServiceImpl.java
+++ src/main/java/org/bts/app/service/impl/TicketServiceImpl.java
@@ -5,12 +5,11 @@ import org.bts.app.dto.BookingRequestDTO;
 import org.bts.app.dto.BookingResponseDTO;
 import org.bts.app.dto.TripDetailsDTO;
 import org.bts.app.model.Seat;
-import org.bts.app.model.SeatStatus;
 import org.bts.app.service.TicketService;
+import org.bts.app.storage.Storage;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
@@ -21,11 +20,11 @@ import static org.bts.app.model.SeatStatus.RESERVED;
 
 public class TicketServiceImpl implements TicketService {
 
-    private static final ConcurrentHashMap<String, Seat> SEATS = seatsInitialization();
+    private static final ConcurrentHashMap<String, Seat> SEATS = Storage.seatsInitialization();
 
-    private static final Map<String, Map<String, Integer[]>> segmentStatusIndexes = segmentStatusIndexInitialization();
+    private static final Map<String, Map<String, Integer[]>> segmentStatusIndexes = Storage.segmentStatusIndexInitialization();
 
-    private static final Map<String, Map<String, Double>> priceWithRoute = priceWithRouteInitialization();
+    private static final Map<String, Map<String, Double>> priceWithRoute = Storage.priceWithRouteInitialization();
 
     @Override
     public AvailabilityResponseDTO checkAvailability(int passengerCount, String origin, String destination, String travelDate) {
@@ -192,78 +191,6 @@ public class TicketServiceImpl implements TicketService {
             throw new IllegalArgumentException("route not found: " + origin + " -> " + destination);
         }
         return routePrice * passengerCount;
-    }
-
-
-    private static Map<String, Map<String, Integer[]>> segmentStatusIndexInitialization() {
-
-        Map<String, Map<String, Integer[]>> segmentIndex = new HashMap<>();
-
-        Map<String, Integer[]> fromA = new HashMap<>();
-        fromA.put("B", new Integer[]{0});
-        fromA.put("C", new Integer[]{0, 1});
-        fromA.put("D", new Integer[]{0, 1, 2});
-        segmentIndex.put("A", fromA);
-
-        Map<String, Integer[]> fromB = new HashMap<>();
-        fromB.put("C", new Integer[]{1});
-        fromB.put("D", new Integer[]{1, 2});
-        fromB.put("A", new Integer[]{5});
-        segmentIndex.put("B", fromB);
-
-        Map<String, Integer[]> fromC = new HashMap<>();
-        fromC.put("D", new Integer[]{2});
-        fromC.put("B", new Integer[]{4});
-        fromC.put("A", new Integer[]{4, 5});
-        segmentIndex.put("C", fromC);
-
-        Map<String, Integer[]> fromD = new HashMap<>();
-        fromD.put("C", new Integer[]{3});
-        fromD.put("B", new Integer[]{3, 4});
-        fromD.put("A", new Integer[]{3, 4, 5});
-        segmentIndex.put("D", fromD);
-
-        return segmentIndex;
-    }
-
-    private static  Map<String, Map<String, Double>> priceWithRouteInitialization() {
-        Map<String, Map<String, Double>> priceWithRoute = new HashMap<>();
-
-        Map<String, Double> fromA = new HashMap<>();
-        fromA.put("B", 50.0); // A --> B
-        fromA.put("C", 100.0);
-        fromA.put("D", 150.0);
-        priceWithRoute.put("A", fromA);
-
-        Map<String, Double> fromB = new HashMap<>();
-        fromB.put("C", 50.0); // B --> C
-        fromB.put("D", 100.0);
-        priceWithRoute.put("B", fromB);
-
-        Map<String, Double> fromC = new HashMap<>();
-        fromC.put("D", 50.0); // C --> D
-        priceWithRoute.put("C", fromC);
-
-        return priceWithRoute;
-    }
-
-    private static ConcurrentHashMap<String, Seat> seatsInitialization() {
-        ConcurrentHashMap<String, Seat> seats = new ConcurrentHashMap<>();
-
-        for(char row='A'; row<='J'; row++){
-            for(int i=1;i<5;i++){
-                String seatId = row + "" + i;
-
-                Seat seat = new Seat();
-                seat.setSeatId(seatId);
-                seat.setRow(String.valueOf(row));
-                seat.setColumn(i);
-                SeatStatus[] segmentStatusInit = {AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE};
-                seat.setSegmentStatus(segmentStatusInit);
-                seats.put(seatId, seat);
-            }
-        }
-        return seats;
     }
 
 }
---
+++ src/main/java/org/bts/app/storage/Storage.java
@@ -0,0 +1,84 @@
+package org.bts.app.storage;
+
+import org.bts.app.model.Seat;
+import org.bts.app.model.SeatStatus;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static org.bts.app.model.SeatStatus.AVAILABLE;
+
+public final class Storage {
+
+    public static Map<String, Map<String, Integer[]>> segmentStatusIndexInitialization() {
+
+        Map<String, Map<String, Integer[]>> segmentIndex = new HashMap<>();
+
+        Map<String, Integer[]> fromA = new HashMap<>();
+        fromA.put("B", new Integer[]{0});
+        fromA.put("C", new Integer[]{0, 1});
+        fromA.put("D", new Integer[]{0, 1, 2});
+        segmentIndex.put("A", fromA);
+
+        Map<String, Integer[]> fromB = new HashMap<>();
+        fromB.put("C", new Integer[]{1});
+        fromB.put("D", new Integer[]{1, 2});
+        fromB.put("A", new Integer[]{5});
+        segmentIndex.put("B", fromB);
+
+        Map<String, Integer[]> fromC = new HashMap<>();
+        fromC.put("D", new Integer[]{2});
+        fromC.put("B", new Integer[]{4});
+        fromC.put("A", new Integer[]{4, 5});
+        segmentIndex.put("C", fromC);
+
+        Map<String, Integer[]> fromD = new HashMap<>();
+        fromD.put("C", new Integer[]{3});
+        fromD.put("B", new Integer[]{3, 4});
+        fromD.put("A", new Integer[]{3, 4, 5});
+        segmentIndex.put("D", fromD);
+
+        return segmentIndex;
+    }
+
+    public static  Map<String, Map<String, Double>> priceWithRouteInitialization() {
+        Map<String, Map<String, Double>> priceWithRoute = new HashMap<>();
+
+        Map<String, Double> fromA = new HashMap<>();
+        fromA.put("B", 50.0); // A --> B
+        fromA.put("C", 100.0);
+        fromA.put("D", 150.0);
+        priceWithRoute.put("A", fromA);
+
+        Map<String, Double> fromB = new HashMap<>();
+        fromB.put("C", 50.0); // B --> C
+        fromB.put("D", 100.0);
+        priceWithRoute.put("B", fromB);
+
+        Map<String, Double> fromC = new HashMap<>();
+        fromC.put("D", 50.0); // C --> D
+        priceWithRoute.put("C", fromC);
+
+        return priceWithRoute;
+    }
+
+    public static ConcurrentHashMap<String, Seat> seatsInitialization() {
+        ConcurrentHashMap<String, Seat> seats = new ConcurrentHashMap<>();
+
+        for(char row='A'; row<='J'; row++){
+            for(int i=1;i<5;i++){
+                String seatId = row + "" + i;
+
+                Seat seat = new Seat();
+                seat.setSeatId(seatId);
+                seat.setRow(String.valueOf(row));
+                seat.setColumn(i);
+                SeatStatus[] segmentStatusInit = {AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE, AVAILABLE};
+                seat.setSegmentStatus(segmentStatusInit);
+                seats.put(seatId, seat);
+            }
+        }
+        return seats;
+    }
+}
